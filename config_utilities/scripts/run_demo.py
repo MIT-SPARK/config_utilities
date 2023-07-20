2dfc509702cb2106df63ced4f3c5b3d5c2513085
Nathan Hughes <nathan.h.hughes@gmail.com>
2023-07-20 22:25:29 +0000

    add shim for running demo script and don't install demos

---
+++ config_utilities/scripts/run_demo.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""Shim script for running one of the demo executables."""
+import pathlib
+import subprocess
+import argparse
+import sys
+
+
+def _autodetect_build_path():
+    return None
+
+
+def _get_resource_path():
+    project_dir = pathlib.Path(__file__).absolute().parent.parent
+    return project_dir / "demos" / "resources"
+
+
+def _run_demo(build_path, name, resource_path):
+    subprocess.run([str(build_path / name), str(resource_path)])
+
+
+def main():
+    """Run script."""
+    parser = argparse.ArgumentParser(description="shim for running demo executables")
+    parser.add_argument("name", help="executable name")
+    parser.add_argument(
+        "--build_path", "-b", help="path to build directory", nargs="?", default=None
+    )
+    args = parser.parse_args()
+
+    resource_path = _get_resource_path()
+    print(f"using resource path: {resource_path}")
+
+    if not args.build_path:
+        build_path = _autodetect_build_path()
+        if not build_path:
+            print("Unable to detect build path! Specify manually with -b/--build_path")
+            sys.exit(1)
+    else:
+        build_path = pathlib.Path(args.build_path)
+
+    if not build_path.exists():
+        print(f"{build_path} does not exist")
+        sys.exit(1)
+
+    executable_path = build_path / "demos" / f"demo_{args.name}"
+    subprocess.run([str(executable_path), str(resource_path)])
+
+
+if __name__ == "__main__":
+    main()
