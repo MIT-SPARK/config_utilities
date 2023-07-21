c41c6c01d0721fa9a10d2553cb363c2a92031a2e
Nathan Hughes <nathan.h.hughes@gmail.com>
2023-07-21 13:20:42 +0000

    handle glog and clean up optional package macros more

--- config_utilities/cmake/OptionalPackage.cmake
+++ config_utilities/cmake/OptionalPackage.cmake
@@ -1,10 +1,47 @@
-macro(FIND_OPTIONAL package_name package_option)
-  find_package(${package_name})
-  if(${package_name}_FOUND)
+# Finds an optional dependency gated on an enable flag
+#
+# Sets ENABLE_${package_name} to TRUE or FALSE depending on availability.
+# All results of find_package(package_name) are available if
+# ENABLE_${package_name} is TRUE
+#
+# Args:
+#   package_name: Name of the package to find
+#   package_enabled: Whether or not the package should be used
+macro(FIND_OPTIONAL package_name package_enabled)
+  if(${package_enabled})
+    find_package(${package_name})
+  endif()
+
+  if(${${package_name}_FOUND})
     set(ENABLE_${package_name} TRUE)
   else()
     set(ENABLE_${package_name} FALSE)
-    if(${package_option})
+    if(${package_enabled})
+      message(WARNING "${package_name} not found. Consider disabling")
+    endif()
+  endif()
+endmacro()
+
+# Finds an optional dependency gated on an enable flag via PkgConfig.
+#
+# Sets ENABLE_${package_name} to TRUE or FALSE depending on availability.
+# The dependency will be available under PkgConfig::package_name
+#
+# Args:
+#   package_name: Name of the package to find
+#   package_enabled: Whether or not the package should be used
+macro(FIND_OPTIONAL_PKGCFG package_name package_enabled)
+  unset(${package_name}_FOUND CACHE)
+  if(${package_enabled})
+    find_package(PkgConfig REQUIRED)
+    pkg_check_modules(${package_name} IMPORTED_TARGET ${package_name})
+  endif()
+
+  if(${${package_name}_FOUND})
+    set(ENABLE_${package_name} TRUE)
+  else()
+    set(ENABLE_${package_name} FALSE)
+    if(${package_enabled})
       message(WARNING "${package_name} not found. Consider disabling")
     endif()
   endif()
