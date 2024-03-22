660dca1890497faab33d6788f67b59e31a14a338
Nathan Hughes <nathan.h.hughes@gmail.com>
2024-03-22 02:00:42 +0000

    readd function for downstream dependencies that use it

--- config_utilities/include/config_utilities/globals.h
+++ config_utilities/include/config_utilities/globals.h
@@ -1,3 +1,9 @@
 #pragma once
+#include <string>
+#include <exception>
 
 // TODO(nathan) drop once dependencies don't include this
+
+namespace config {
+std::string printAllValidConfigs(bool flag) { throw std::runtime_error("deprecated"); }
+}  // namespace config
