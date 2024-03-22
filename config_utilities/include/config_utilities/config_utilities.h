5c0da88450eb8e2aa03b044c2d436816489c4263
Nathan Hughes <nathan.h.hughes@gmail.com>
2024-03-22 01:52:20 +0000

    drop missing header

--- config_utilities/include/config_utilities/config_utilities.h
+++ config_utilities/include/config_utilities/config_utilities.h
@@ -41,7 +41,6 @@
 #include "config_utilities/config.h"
 #include "config_utilities/factory.h"
 #include "config_utilities/formatting/asl.h"
-#include "config_utilities/globals.h"
 #include "config_utilities/logging/log_to_stdout.h"
 #include "config_utilities/printing.h"
 #include "config_utilities/settings.h"
