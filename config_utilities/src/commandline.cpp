d640177827581e7f8924649bb97882360b879102
Nathan Hughes <nathan.h.hughes@gmail.com>
2025-01-13 16:41:34 +0000

    add command line tests and fix application order

--- config_utilities/src/commandline.cpp
+++ config_utilities/src/commandline.cpp
@@ -46,26 +46,32 @@ namespace internal {
 
 namespace fs = std::filesystem;
 
+struct Span {
+  int pos = 0;
+  int num_tokens = 1;
+  std::string key;
+
+  std::string extractTokens(int argc, char* argv[]) const;
+};
+
 struct CliParser {
+  struct Entry {
+    std::string value;
+    bool is_file;
+  };
+
   static constexpr auto FILE_OPT = "--config-utilities-file";
   static constexpr auto YAML_OPT = "--config-utilities-yaml";
 
   CliParser() = default;
   CliParser& parse(int& argc, char* argv[], bool remove_args);
 
-  std::vector<std::string> files;
-  std::vector<std::string> yaml_entries;
-};
-
-struct Span {
-  int pos = 0;
-  int num_tokens = 1;
-  std::string extractTokens(int argc, char* argv[]) const;
+  std::vector<Entry> entries;
 };
 
 std::string Span::extractTokens(int argc, char* argv[]) const {
   if (pos < 0) {
-    return "";
+    return "";  // NOTE(nathan) unreachable
   }
 
   std::stringstream ss;
@@ -80,34 +86,29 @@ std::string Span::extractTokens(int argc, char* argv[]) const {
   return ss.str();
 }
 
-std::ostream& operator<<(std::ostream& out, const Span& span) {
-  out << "[" << span.pos << ", " << span.pos + span.num_tokens << "]";
-  return out;
-}
-
 std::optional<Span> getSpan(int argc, char* argv[], int pos, bool parse_all, std::string& error) {
   int index = pos + 1;
   while (index < argc) {
     const std::string curr_opt = argv[index];
     const bool is_flag = !curr_opt.empty() && curr_opt[0] == '-';
-    if (is_flag && index == pos + 1) {
-      error = parse_all ? "at least one value required!" : "missing required value!";
-      return std::nullopt;
+    if (is_flag) {
+      break;  // stop parsing the span
     }
 
     if (!parse_all) {
-      return Span{pos, 1};
-    }
-
-    if (is_flag) {
-      break;  // stop parsing the span
+      return Span{pos, 1, argv[pos]};
     }
 
     ++index;
   }
 
+  if (index == pos + 1) {
+    error = parse_all ? "at least one value required!" : "missing required value!";
+    return std::nullopt;
+  }
+
   // return multi-token span
-  return Span{pos, index - pos - 1};
+  return Span{pos, index - pos - 1, argv[pos]};
 }
 
 void removeSpan(int& argc, char* argv[], const Span& span) {
@@ -121,22 +122,8 @@ void removeSpan(int& argc, char* argv[], const Span& span) {
   argc -= span.num_tokens + 1;
 }
 
-void removeSpans(int& argc, char* argv[], const std::map<std::string, std::vector<Span>>& arg_spans) {
-  std::vector<Span> spans;
-  for (const auto& [key, key_spans] : arg_spans) {
-    for (const auto& span : key_spans) {
-      spans.push_back(span);
-    }
-  }
-
-  std::sort(spans.begin(), spans.end(), [](const auto& lhs, const auto& rhs) { return lhs.pos > rhs.pos; });
-  for (const auto& span : spans) {
-    removeSpan(argc, argv, span);
-  }
-}
-
 CliParser& CliParser::parse(int& argc, char* argv[], bool remove_args) {
-  std::map<std::string, std::vector<Span>> spans;
+  std::vector<Span> spans;
 
   int i = 0;
   while (i < argc) {
@@ -148,12 +135,7 @@ CliParser& CliParser::parse(int& argc, char* argv[], bool remove_args) {
     }
 
     if (curr_span) {
-      auto iter = spans.find(curr_opt);
-      if (iter == spans.end()) {
-        iter = spans.emplace(curr_opt, std::vector<Span>()).first;
-      }
-
-      iter->second.emplace_back(*curr_span);
+      spans.emplace_back(*curr_span);
       i += curr_span->num_tokens;
       continue;
     }
@@ -166,70 +148,79 @@ CliParser& CliParser::parse(int& argc, char* argv[], bool remove_args) {
     ++i;
   }
 
-  for (const auto& [key, key_spans] : spans) {
-    for (const auto& span : key_spans) {
-      if (key == FILE_OPT) {
-        files.push_back(span.extractTokens(argc, argv));
-      } else {
-        yaml_entries.push_back(span.extractTokens(argc, argv));
-      }
-    }
+  for (const auto& span : spans) {
+    entries.push_back(Entry{span.extractTokens(argc, argv), span.key == FILE_OPT});
   }
 
   if (remove_args) {
-    removeSpans(argc, argv, spans);
+    std::sort(spans.begin(), spans.end(), [](const auto& lhs, const auto& rhs) { return lhs.pos > rhs.pos; });
+    for (const auto& span : spans) {
+      removeSpan(argc, argv, span);
+    }
   }
 
   return *this;
 }
 
-YAML::Node loadFromArguments(int& argc, char* argv[], bool remove_args) {
-  YAML::Node node;
+YAML::Node nodeFromFileEntry(const CliParser::Entry& entry) {
+  auto filepath = entry.value;
 
-  const auto parser = CliParser().parse(argc, argv, remove_args);
-
-  for (const auto& entry : parser.files) {
-    auto filepath = entry;
-    std::string ns;
-    const auto index = filepath.find("@");
-    if (index != std::string::npos) {
-      ns = filepath.substr(index + 1);
-      filepath = filepath.substr(0, index);
-    }
-
-    std::filesystem::path file(filepath);
-    if (!fs::exists(file)) {
-      // TODO(nathan) log instead of manual print
-      std::cerr << "File " << file << " does not exist!" << std::endl;
-      continue;
-    }
-
-    YAML::Node file_node;
-    try {
-      file_node = YAML::LoadFile(file);
-    } catch (const std::exception& e) {
-      // TODO(nathan) log instead of manual print
-      std::cerr << "Failure for " << file << ": " << e.what() << std::endl;
-      continue;
-    }
-
-    if (!ns.empty()) {
-      internal::moveDownNamespace(file_node, ns);
-    }
-
-    internal::mergeYamlNodes(node, file_node, true);
+  std::string ns;
+  const auto index = filepath.find("@");
+  if (index != std::string::npos) {
+    ns = filepath.substr(index + 1);
+    filepath = filepath.substr(0, index);
   }
 
-  for (const auto& entry : parser.yaml_entries) {
-    YAML::Node cli_node;
-    try {
-      cli_node = YAML::Load(entry);
-    } catch (const std::exception& e) {
-      // TODO(nathan) log instead of manual print
-      std::cerr << "Failure for '" << entry << "': " << e.what() << std::endl;
+  YAML::Node node;
+  std::filesystem::path file(filepath);
+  if (!fs::exists(file)) {
+    // TODO(nathan) log instead of manual print
+    std::cerr << "File " << file << " does not exist!" << std::endl;
+    return node;
+  }
+
+  try {
+    node = YAML::LoadFile(file);
+  } catch (const std::exception& e) {
+    // TODO(nathan) log instead of manual print
+    std::cerr << "Failure for " << file << ": " << e.what() << std::endl;
+    return node;
+  }
+
+  if (!ns.empty()) {
+    internal::moveDownNamespace(node, ns);
+  }
+
+  return node;
+}
+
+YAML::Node nodeFromLiteralEntry(const CliParser::Entry& entry) {
+  YAML::Node node;
+  try {
+    node = YAML::Load(entry.value);
+  } catch (const std::exception& e) {
+    // TODO(nathan) log instead of manual print
+    std::cerr << "Failure for '" << entry.value << "': " << e.what() << std::endl;
+  }
+
+  return node;
+}
+
+YAML::Node loadFromArguments(int& argc, char* argv[], bool remove_args) {
+  const auto parser = CliParser().parse(argc, argv, remove_args);
+
+  YAML::Node node;
+  for (const auto& entry : parser.entries) {
+    YAML::Node parsed_node;
+    if (entry.is_file) {
+      parsed_node = nodeFromFileEntry(entry);
+    } else {
+      parsed_node = nodeFromLiteralEntry(entry);
     }
 
-    internal::mergeYamlNodes(node, cli_node, true);
+    // no-op for invalid parsed node
+    internal::mergeYamlNodes(node, parsed_node, true);
   }
 
   return node;
