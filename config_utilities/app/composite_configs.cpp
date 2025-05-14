7a8bc71982413d7e25b093c4d9a2507c443357ab
Nathan Hughes <nathan.h.hughes@gmail.com>
2025-05-14 15:32:02 -0400

    expand command line help doc

--- config_utilities/app/composite_configs.cpp
+++ config_utilities/app/composite_configs.cpp
@@ -10,18 +10,20 @@ Invalid YAML or missing files get dropped during compositing.
 
 Options:
   -h/--help: Show this message.
-  --config-utilities-yaml: Takes an arbitrary set of tokens that form a valid YAML string.
-                           Spaces are not required to be escaped, so `--config-utilities foo: value`
-                           is parsed the same as `--config-utilities 'foo: value'`. Can be specified
-                           multiple times.
-  --config-utilities-file: Takes a filepath to YAML to load and composite. The YAML can optionally
-                           be namespaced by `@NAMESPACE` where 'FILE@a/b' maps to
-                           '{a: {b: FILE_CONTENTS}}'. Can be specified multiple times.
+  -c/--config-utilities-yaml: Takes an arbitrary set of tokens that form a valid YAML string.
+                              Spaces are not required to be escaped, so `--config-utilities foo: value`
+                              is parsed the same as `--config-utilities 'foo: value'`. Can be specified
+                              multiple times.
+  -f/--config-utilities-file: Takes a filepath to YAML to load and composite. The YAML can optionally
+                              be namespaced by `@NAMESPACE` where 'FILE@a/b' maps to
+                              '{a: {b: FILE_CONTENTS}}'. Can be specified multiple times.
+  -v/--config-utilities-var: Takes a KEY=VALUE pair to add to the substitution context. Can be specified
+                             multiple times.
 
 Example:
 > echo "{a: 42, bar: hello}" > /tmp/test_in.yaml
-> composite-configs --config-utilities-yaml "{foo: {bar: value, b: -1.0}}" --config-utilities-file /tmp/test_in.yaml@foo
-{foo: {bar: hello, b: -1.0, a: 42}}
+> composite-configs --config-utilities-yaml "{foo: {bar: value, b: -1.0, c: $<var other>}}" --config-utilities-file /tmp/test_in.yaml@foo -v other=42
+{foo: {bar: hello, b: -1.0, c: 42, a: 42}}
 
 See https://github.com/MIT-SPARK/config_utilities/blob/main/docs/Parsing.md#parse-from-the-command-line
 for more information.
