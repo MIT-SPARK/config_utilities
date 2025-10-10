5a5f4c2e7754dd5bf7b9a67117de3788ea6167ed
Nathan Hughes <nathan.h.hughes@gmail.com>
2025-10-10 09:28:20 -0400

    drop deprecated functions and files

--- config_utilities/include/config_utilities/globals.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-#include <exception>
-
-// TODO(nathan) drop once dependencies don't include this
-
-namespace config {
-std::string printAllValidConfigs(bool flag) { throw std::runtime_error("deprecated"); }
-}  // namespace config
