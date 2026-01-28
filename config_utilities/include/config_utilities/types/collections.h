d952421d9e32b00d350232b5d80b1d99b1502be5
Nathan Hughes <nathan.h.hughes@gmail.com>
2026-01-28 12:44:08 -0500

    Feature/collection conversions (#70)
    * (wip) start on converter for collection types
    
    * fix templating and add unit test
    
    * make converters more generic and add value and sequence variants
    
    * add docstring
    
    * add full map conversion

---
+++ config_utilities/include/config_utilities/types/collections.h
@@ -0,0 +1,280 @@
+/** -----------------------------------------------------------------------------
+ * Copyright (c) 2023 Massachusetts Institute of Technology.
+ * All Rights Reserved.
+ *
+ * AUTHORS:     Lukas Schmid <lschmid@mit.edu>, Nathan Hughes <na26933@mit.edu>
+ * AFFILIATION: MIT-SPARK Lab, Massachusetts Institute of Technology
+ * YEAR:        2023
+ * LICENSE:     BSD 3-Clause
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * -------------------------------------------------------------------------- */
+
+#pragma once
+
+#include <string>
+
+namespace config {
+namespace detail {
+
+template <class FuncT>
+struct ConvertArgs;
+
+template <typename OrigT, typename InterT>
+struct ConvertArgs<void(InterT, OrigT, std::string&)> {
+  using intermediate = std::remove_const_t<std::remove_reference_t<InterT>>;
+  using original = std::remove_const_t<std::remove_reference_t<OrigT>>;
+};
+
+template <typename Converter>
+using intermediate_t = typename ConvertArgs<decltype(Converter::fromIntermediate)>::intermediate;
+
+template <typename Converter>
+using original_t = typename ConvertArgs<decltype(Converter::fromIntermediate)>::original;
+
+}  // namespace detail
+
+/**
+ * @brief Field conversion that applies another conversion to keys in a map
+ * @tparam Converter Conversion to apply to every key in the map
+ *
+ * Automatic type inference *should* work as long as the conversion is specified, e.g.,
+ * `field<MapKeyConverter<ConverterToApply>>(config.field, "field");`
+ * Can be used for any "associative" collection type that exposes a forward iterator and `emplace()`, e.g., `std::map`
+ * or `std::unordered_map`.
+ *
+ * Note that keys that result in a conversion error will be dropped
+ */
+template <typename Converter>
+struct MapKeyConverter {
+  using InterT = detail::intermediate_t<Converter>;
+  using KeyT = detail::original_t<Converter>;
+
+  template <typename ValueT, template <typename K, typename V> typename Map>
+  static Map<InterT, ValueT> toIntermediate(const Map<KeyT, ValueT> original, std::string& error) {
+    Map<InterT, ValueT> intermediate;
+    for (const auto& [key, value] : original) {
+      std::string key_error;
+      auto new_key = Converter::toIntermediate(key, key_error);
+      if (!key_error.empty()) {
+        error += " key conversion failure: " + key_error;
+        continue;
+      }
+
+      intermediate.emplace(new_key, value);
+    }
+
+    return intermediate;
+  }
+
+  template <typename ValueT, template <typename K, typename V> typename Map>
+  static void fromIntermediate(const Map<InterT, ValueT>& intermediate,
+                               Map<InterT, ValueT>& original,
+                               std::string& error) {
+    original.clear();
+    for (const auto& [key, value] : intermediate) {
+      std::string key_error;
+      KeyT new_key;
+      Converter::fromIntermediate(key, new_key, key_error);
+      if (!key_error.empty()) {
+        error += " key conversion failure: " + key_error;
+        continue;
+      }
+
+      original.emplace(new_key, value);
+    }
+  }
+};
+
+/**
+ * @brief Field conversion that applies another conversion to values in a map
+ * @tparam Converter Conversion to apply to every value in the map
+ *
+ * Automatic type inference *should* work as long as the conversion is specified, e.g.,
+ * `field<MapValueConverter<ConverterToApply>>(config.field, "field");`
+ * Can be used for any "associative" collection type that exposes a forward iterator and `emplace()`, e.g., `std::map`
+ * or `std::unordered_map`.
+ *
+ * Note that values that result in a conversion error will be dropped
+ */
+template <typename Converter>
+struct MapValueConverter {
+  using InterT = detail::intermediate_t<Converter>;
+  using ValueT = detail::original_t<Converter>;
+
+  template <typename KeyT, template <typename K, typename V> typename Map>
+  static Map<KeyT, InterT> toIntermediate(const Map<KeyT, ValueT> original, std::string& error) {
+    Map<KeyT, InterT> intermediate;
+    for (const auto& [key, value] : original) {
+      std::string value_error;
+      auto new_value = Converter::toIntermediate(value, value_error);
+      if (!value_error.empty()) {
+        error += " value conversion failure: " + value_error;
+        continue;
+      }
+
+      intermediate.emplace(key, new_value);
+    }
+
+    return intermediate;
+  }
+
+  template <typename KeyT, template <typename K, typename V> typename Map>
+  static void fromIntermediate(const Map<KeyT, InterT>& intermediate, Map<KeyT, ValueT>& original, std::string& error) {
+    original.clear();
+    for (const auto& [key, value] : intermediate) {
+      std::string value_error;
+      ValueT new_value;
+      Converter::fromIntermediate(value, new_value, value_error);
+      if (!value_error.empty()) {
+        error += " value conversion failure: " + value_error;
+        continue;
+      }
+
+      original.emplace(key, new_value);
+    }
+  }
+};
+
+/**
+ * @brief Field conversion that applies conversions to keys and values in a map
+ * @tparam KeyConverter Conversion to apply to every key in the map
+ * @tparam ValueConverter Conversion to apply to every value in the map
+ *
+ * Can be used for any "associative" collection type that exposes a forward iterator and `emplace()`, e.g., `std::map`
+ * or `std::unordered_map`, which should be inferred automatically.
+ *
+ * Note that keys or values that result in a conversion error will be dropped
+ */
+template <typename KeyConverter, typename ValueConverter>
+struct MapKeyValueConverter {
+  using InterKeyT = detail::intermediate_t<KeyConverter>;
+  using OrigKeyT = detail::original_t<KeyConverter>;
+  using InterValueT = detail::intermediate_t<ValueConverter>;
+  using OrigValueT = detail::original_t<ValueConverter>;
+
+  template <template <typename K, typename V> typename Map>
+  static Map<InterKeyT, InterValueT> toIntermediate(const Map<OrigKeyT, OrigValueT> original, std::string& error) {
+    Map<InterKeyT, InterValueT> intermediate;
+    for (const auto& [key, value] : original) {
+      std::string key_error;
+      auto new_key = KeyConverter::toIntermediate(key, key_error);
+      if (!key_error.empty()) {
+        error += " key conversion failure: " + key_error;
+        continue;
+      }
+
+      std::string value_error;
+      auto new_value = ValueConverter::toIntermediate(value, value_error);
+      if (!value_error.empty()) {
+        error += " value conversion failure: " + value_error;
+        continue;
+      }
+
+      intermediate.emplace(new_key, new_value);
+    }
+
+    return intermediate;
+  }
+
+  template <template <typename K, typename V> typename Map>
+  static void fromIntermediate(const Map<InterKeyT, InterValueT>& intermediate,
+                               Map<OrigKeyT, OrigValueT>& original,
+                               std::string& error) {
+    original.clear();
+    for (const auto& [key, value] : intermediate) {
+      std::string key_error;
+      OrigKeyT new_key;
+      KeyConverter::fromIntermediate(key, new_key, key_error);
+      if (!key_error.empty()) {
+        error += " key conversion failure: " + key_error;
+        continue;
+      }
+
+      std::string value_error;
+      OrigKeyT new_value;
+      ValueConverter::fromIntermediate(value, new_value, value_error);
+      if (!value_error.empty()) {
+        error += " value conversion failure: " + value_error;
+        continue;
+      }
+
+      original.emplace(new_key, new_value);
+    }
+  }
+};
+
+/**
+ * @brief Field conversion that applies another conversion to values in a sequence
+ * @tparam Converter Conversion to apply to every value in the sequence
+ *
+ * Automatic type inference *should* work as long as the conversion is specified, e.g.,
+ * `field<SequenceConverter<ConverterToApply>>(config.field, "field");`
+ * Can be used for any collection type that exposes a forward iterator and `emplace_back()`, e.g., `std::vector` or
+ * `std::list`.
+ *
+ * Note that values that result in a conversion error will be dropped
+ */
+template <typename Converter>
+struct SequenceConverter {
+  using OrigT = detail::original_t<Converter>;
+  using InterT = detail::intermediate_t<Converter>;
+
+  template <template <typename T> typename Seq>
+  static Seq<InterT> toIntermediate(const Seq<OrigT> original, std::string& error) {
+    Seq<InterT> intermediate;
+    for (const auto& orig : original) {
+      std::string value_error;
+      auto new_value = Converter::toIntermediate(orig, value_error);
+      if (!value_error.empty()) {
+        error += " value conversion failure: " + value_error;
+        continue;
+      }
+
+      intermediate.emplace_back(new_value);
+    }
+
+    return intermediate;
+  }
+
+  template <template <typename T> typename Seq>
+  static void fromIntermediate(const Seq<InterT>& intermediate, Seq<OrigT>& original, std::string& error) {
+    original.clear();
+    for (const auto& value : intermediate) {
+      std::string value_error;
+      OrigT new_value;
+      Converter::fromIntermediate(value, new_value, value_error);
+      if (!value_error.empty()) {
+        error += " value conversion failure: " + value_error;
+        continue;
+      }
+
+      original.emplace_back(new_value);
+    }
+  }
+};
+
+}  // namespace config
---
+++ config_utilities/test/tests/collections.cpp
@@ -0,0 +1,191 @@
+/** -----------------------------------------------------------------------------
+ * Copyright (c) 2023 Massachusetts Institute of Technology.
+ * All Rights Reserved.
+ *
+ * AUTHORS:     Lukas Schmid <lschmid@mit.edu>, Nathan Hughes <na26933@mit.edu>
+ * AFFILIATION: MIT-SPARK Lab, Massachusetts Institute of Technology
+ * YEAR:        2023
+ * LICENSE:     BSD 3-Clause
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * -------------------------------------------------------------------------- */
+
+#include "config_utilities/types/collections.h"
+
+#include <list>
+#include <map>
+#include <unordered_map>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#include "config_utilities/config.h"
+#include "config_utilities/parsing/yaml.h"
+
+namespace config::test {
+
+struct PlusOneConversion {
+  static int toIntermediate(int value, std::string&) { return value + 1; }
+  static void fromIntermediate(int intermediate, int& value, std::string&) { value = intermediate - 1; }
+};
+
+struct MinusOneConversion {
+  static int toIntermediate(int value, std::string&) { return value - 1; }
+  static void fromIntermediate(int intermediate, int& value, std::string&) { value = intermediate + 1; }
+};
+
+struct KeyConversionStruct {
+  std::map<int, std::string> key_map;
+  std::unordered_map<int, std::string> unordered_key_map;
+
+  std::map<int, std::string> ordered_key_map() const {
+    return std::map<int, std::string>(unordered_key_map.begin(), unordered_key_map.end());
+  }
+};
+
+void declare_config(KeyConversionStruct& config) {
+  field<MapKeyConverter<PlusOneConversion>>(config.key_map, "key_map");
+  field<MapKeyConverter<PlusOneConversion>>(config.unordered_key_map, "key_map");
+}
+
+struct ValueConversionStruct {
+  std::map<std::string, int> value_map;
+  std::unordered_map<std::string, int> unordered_value_map;
+
+  std::map<std::string, int> ordered_value_map() const {
+    return std::map<std::string, int>(unordered_value_map.begin(), unordered_value_map.end());
+  }
+};
+
+void declare_config(ValueConversionStruct& config) {
+  field<MapValueConverter<PlusOneConversion>>(config.value_map, "value_map");
+  field<MapValueConverter<PlusOneConversion>>(config.unordered_value_map, "value_map");
+}
+
+struct KeyValueConversionStruct {
+  std::map<int, int> int_map;
+  std::unordered_map<int, int> unordered_int_map;
+
+  std::map<int, int> ordered_int_map() const {
+    return std::map<int, int>(unordered_int_map.begin(), unordered_int_map.end());
+  }
+};
+
+void declare_config(KeyValueConversionStruct& config) {
+  using FullConversion = MapKeyValueConverter<PlusOneConversion, MinusOneConversion>;
+  field<FullConversion>(config.int_map, "int_map");
+  field<FullConversion>(config.unordered_int_map, "int_map");
+}
+
+struct SeqConversionStruct {
+  std::list<int> value_list;
+  std::vector<int> value_vec;
+
+  std::list<int> vec_as_list() const { return std::list<int>(value_vec.begin(), value_vec.end()); }
+};
+
+void declare_config(SeqConversionStruct& config) {
+  field<SequenceConverter<PlusOneConversion>>(config.value_list, "values");
+  field<SequenceConverter<PlusOneConversion>>(config.value_vec, "values");
+}
+
+TEST(Collections, ConvertKeys) {
+  auto node = YAML::Load(R"yaml(
+key_map:
+  2: world
+  3: hello
+  0: test
+)yaml");
+  auto result = fromYaml<KeyConversionStruct>(node);
+  std::map<int, std::string> expected{{-1, "test"}, {1, "world"}, {2, "hello"}};
+  EXPECT_EQ(expected, result.key_map);
+  EXPECT_EQ(expected, result.ordered_key_map());
+
+  // make sure second parse overrides first
+  node = YAML::Load(R"yaml(
+key_map:
+  0: foo
+  )yaml");
+  EXPECT_TRUE(updateFromYaml(result, node));
+  expected = {{-1, "foo"}};
+  EXPECT_EQ(expected, result.key_map);
+  EXPECT_EQ(expected, result.ordered_key_map());
+}
+
+TEST(Collections, ConvertValues) {
+  auto node = YAML::Load(R"yaml(
+value_map:
+  world: 2
+  hello: 3
+  test: 0
+)yaml");
+  auto result = fromYaml<ValueConversionStruct>(node);
+  std::map<std::string, int> expected{{"test", -1}, {"world", 1}, {"hello", 2}};
+  EXPECT_EQ(expected, result.value_map);
+  EXPECT_EQ(expected, result.ordered_value_map());
+
+  // make sure second parse overrides first
+  node = YAML::Load(R"yaml(
+value_map:
+  foo: 0
+  )yaml");
+  EXPECT_TRUE(updateFromYaml(result, node));
+  expected = {{"foo", -1}};
+  EXPECT_EQ(expected, result.value_map);
+  EXPECT_EQ(expected, result.ordered_value_map());
+}
+
+TEST(Collections, ConvertFullMap) {
+  auto node = YAML::Load(R"yaml(int_map: {1: 2, 2: 3, 3: 0})yaml");
+  auto result = fromYaml<KeyValueConversionStruct>(node);
+  std::map<int, int> expected{{0, 3}, {1, 4}, {2, 1}};
+  EXPECT_EQ(expected, result.int_map);
+  EXPECT_EQ(expected, result.ordered_int_map());
+
+  // make sure second parse overrides first
+  node = YAML::Load(R"yaml(int_map: {2: -2})yaml");
+  EXPECT_TRUE(updateFromYaml(result, node));
+  expected = {{1, -1}};
+  EXPECT_EQ(expected, result.int_map);
+  EXPECT_EQ(expected, result.ordered_int_map());
+}
+
+TEST(Collections, ConvertSequence) {
+  auto node = YAML::Load(R"yaml(values: [1, 2, 3])yaml");
+  auto result = fromYaml<SeqConversionStruct>(node);
+  std::list<int> expected{0, 1, 2};
+  EXPECT_EQ(expected, result.value_list);
+  EXPECT_EQ(expected, result.vec_as_list());
+
+  // make sure second parse overrides first
+  node = YAML::Load(R"yaml(values: [0, 2, 4, 6])yaml");
+  EXPECT_TRUE(updateFromYaml(result, node));
+  expected = {-1, 1, 3, 5};
+  EXPECT_EQ(expected, result.value_list);
+  EXPECT_EQ(expected, result.vec_as_list());
+}
+
+}  // namespace config::test
